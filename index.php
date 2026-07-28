7efb81d559950eeac1c8ada67095df2f8a3f0028
melissa-mz <mouazmelissa@gmail.com>
2026-07-28 12:05:48 +0100

    Fix: suppression des avatars dans témoignages

--- index.php
+++ index.php
@@ -258,7 +258,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="#D4AF37"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/></svg>
                 </div>
                 <p class="testimonial-text">
-                    "Ouiiii je l'ai récupéré, Merci beaucoup pour votre réactivité ! J'avais commandé un peu tard et j'étais vraiment pressée, mais vous avez assuré en me l'envoyant à temps. Je vous remercie sincèrement pour votre professionnalisme. A très bientôt !"
+                    "Merci beaucoup pour votre réactivité ! J'avais commandé un peu tard et j'étais vraiment pressée, mais vous avez assuré en me l'envoyant à temps. Je vous remercie sincèrement pour votre professionnalisme. A très bientôt !"
                 </p>
             </div>
 
